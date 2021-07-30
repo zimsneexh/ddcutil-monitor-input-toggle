9c5552d5a077194fd72ef114375df1c4e243c998
zimsneexh <zimsneexh@gmail.com>
2021-07-30 22:35:34 +0200

    change from using a file to check current input to using ddcutil aswell

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
+import subprocess
 import os
 
 def main():
 
-    reader = open(os.path.expanduser('~') + "/.config/ddcutil-monitor-switcher/state.txt", "r")
-    state = reader.read()
-  
-    print(state)
-    
-    if("dp" in state):
-        writer = open(os.path.expanduser('~') + "/.config/ddcutil-monitor-switcher/state.txt", "w")
+    command = "sudo ddcutil getvcp 0x60"
+    cmdres = subprocess.getoutput(command)
+
+    if("0x0f" in cmdres):
         os.system("sudo ddcutil setvcp 0x60 0x05")
-        writer.write("hdmi")
-    elif("hdmi" in state):
-        writer = open(os.path.expanduser('~') + "/.config/ddcutil-monitor-switcher/state.txt", "w")
+    elif("0x05" in cmdres):
         os.system("sudo ddcutil setvcp 0x60 0x0f")
-        writer.write("dp")
-    else:
-        print("Unknown state. Check file!")
 
 if __name__ == "__main__":
     main()
